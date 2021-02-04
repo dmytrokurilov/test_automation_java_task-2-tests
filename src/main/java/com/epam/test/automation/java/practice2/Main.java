f182b3c71f432cba58256e5032694a647ec46369
dmytrokurilov <dmytrokurilov@gmail.com>
2021-02-04 11:52:04 +0200

    Task Done

--- src/main/java/com/epam/test/automation/java/practice2/Main.java
+++ src/main/java/com/epam/test/automation/java/practice2/Main.java
@@ -2,33 +2,55 @@ package com.epam.test.automation.java.practice2;
 
 public class Main {
 
-    /**
-     * <summary>
-     * Implement code according to description of task 1.
-     * </summary>
-     */
-    public static int task1(int value) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+    private Main()
+    {
+
     }
 
-    /**
-     * <summary>
-     * Implement code according to description of task 2.
-     * </summary>
-     */
-    public static int task2(int value) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+    public static int task1(int value)
+    {
+        int b;
+        int sum = 0;
+
+        while (value != 0)
+        {
+            b = value % 10;
+            if (b % 2 != 0)
+            {
+                sum = sum + b;
+            }
+            value = value / 10;
+        }
+        return sum;
     }
 
-    /**
-     * <summary>
-     * Implement code according to description of task 3.
-     * </summary>
-     */
-    public static int task3(int value) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+    public static int task2(int value)
+    {
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            if ((value >> i) % 2 == 1)
+                count++;
+        }
+        return count;
+    }
+
+    public static int task3(int value)
+    {
+        int b = value - 1;
+        if (b <= 0)
+            return 0;
+
+        int[] fib = new int[b + 1];
+        fib[0] = 0; fib[1] = 1;
+
+        int sum = fib[0] + fib[1];
+
+        for (int i = 2; i <= b; i++)
+        {
+            fib[i] = fib[i - 1] + fib[i - 2];
+            sum += fib[i];
+        }
+        return sum;
     }
 }
